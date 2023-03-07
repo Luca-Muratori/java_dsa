b64cd88a29b543edc5211e4e334b91b4dbe8f931
“Luca <luca.muratori.94@hotmail.it>
2023-03-07 22:23:53 +0100

    first commit

---
+++ section12/Main.java
@@ -0,0 +1,16 @@
+class Main{
+    public static void main(String[] args) {
+        CircularSinglyLL csll=new CircularSinglyLL();
+        // csll.createCLL(1);
+        // System.out.println(csll.head.value);
+        // System.out.println(csll.head.next.value);
+        csll.insertCSLL(1, 1);
+        csll.insertCSLL(2, 2);
+        csll.insertCSLL(3,3);
+        csll.insertCSLL(4, 4);
+        csll.insertCSLL(5, 3);
+        System.out.println(csll.tail.next.next.value);
+
+        csll.traverseCSLL();
+    }
+}
