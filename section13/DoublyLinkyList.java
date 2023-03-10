d587d86e13f3343ab0f7c186044a4544f67425aa
“Luca-Muratori” <luca.muratori.94@hotmail.it>
2023-03-10 20:59:53 +0100

    Update Main.java

--- section13/DoublyLinkyList.java
+++ section13/DoublyLinkyList.java
@@ -45,4 +45,100 @@ public class DoublyLinkyList {
         }
         size++;
     }
+
+    public void traverse(){
+        if(head!=null){
+            DoublyNode tempNode= head;
+            for (int i = 0; i < size ; i++) {
+                System.out.print(tempNode.value);
+                if(i!=size-1){
+                    System.out.print("-->");
+                }
+                tempNode=tempNode.next;
+            }
+        }else{
+            System.out.println("DLL does not exist");
+        }
+        System.out.println("\n");
+    }
+
+    public void reverseTraverse(){
+        if(head!=null){
+            DoublyNode tempNode=tail;
+            for (int i = 0; i < size; i++) {
+                System.out.print(tempNode.value);
+                if(i!=size-1){
+                    System.out.print("<--");
+                }
+                tempNode=tempNode.prev;
+            }
+        } else {
+            System.out.println("DLL does not exist");
+        }
+        System.out.println("\n");
+    }
+
+    public boolean search(int nodeValue){
+        if(head!=null){
+            DoublyNode tempNode = head;
+            for (int i = 0; i < size; i++) {
+                if(tempNode.value==nodeValue){
+                    System.out.println("Node ("+tempNode.value+") found at location " + i);
+                    return true;
+                }
+               tempNode=tempNode.next;
+            }
+        }
+        System.out.println("node not found");
+        return false;
+    }
+
+    public void delete(int location){
+        if(head==null){
+            System.out.println("dll does not exist");
+            return;
+        } else if(location==0){
+            if(size==1){
+                head=null;
+                tail=null;
+                size--;
+                return;
+            }else{
+                head=head.next;
+                head.prev=null;
+                size--;
+            }
+        } else if(location>=size){
+            DoublyNode tempNode=tail.prev;
+            if(size==1){
+                head=null;
+                tail=null;
+                size--;
+                return;
+            }else{
+                tempNode.next=null;
+                tail=tempNode;
+                size--;
+            }
+        } else {
+            DoublyNode tempNode = head;
+            for (int i = 0; i < location-1; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next=tempNode.next.next;
+            tempNode.next.prev=tempNode;
+            size--;
+        }
+    }
+
+    public void deleteAll(){
+        DoublyNode tempNode=head;
+        for (int i = 0; i < size; i++) {
+            tempNode.prev=null;
+            tempNode=tempNode.next;
+        }
+        head=null;
+        tail=null;
+        System.out.println("the dll has been deleted");
+    }
 }
--- section13/Main.java
+++ section13/Main.java
@@ -2,11 +2,22 @@ public class Main{
     public static void main(String[] args) {
         DoublyLinkyList dll=new DoublyLinkyList();
         // dll.create(3);
+        
         // System.out.println(dll.head.value);
         dll.insert(1, 0);
         dll.insert(2, 1);
         dll.insert(3, 2);
         dll.insert(4, 3);
+        dll.insert(5, 2);
+        dll.insert(7, 4);
+        dll.traverse();
+        dll.reverseTraverse();
+        dll.search(6);
+        dll.search(2);
+        dll.delete(3);
+        dll.traverse();
+        dll.deleteAll();
+        dll.traverse();
 
     }
 }
