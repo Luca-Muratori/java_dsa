f4e6a800f8735324b96ebd38f7c8e8dbc0fc0f44
“Luca-Muratori” <luca.muratori.94@hotmail.it>
2023-03-30 22:03:30 +0200

    finished section21

--- section21/AVL.java
+++ section21/AVL.java
@@ -39,7 +39,7 @@ public class AVL {
         queue.add(root);
         while (!queue.isEmpty()) {
             BinaryNode presentNode = queue.remove();
-            System.out.println(presentNode.value + " ");
+            System.out.print(presentNode.value + " ");
             if (presentNode.left != null) {
                 queue.add(presentNode.left);
             }
@@ -62,4 +62,142 @@ public class AVL {
             return search(node.right, value);
         }
     }
+
+    // insertion, for it we could need a rotation method
+    // first we create the helper method
+    // we need the height of a node, to check if there is some unbalance
+    public int getHeight(BinaryNode node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return node.height;
+        }
+    }
+
+    // rotate right method
+    private BinaryNode rotateRight(BinaryNode disbalanceNode) {// tc o1---sc o1
+        BinaryNode newRoot = disbalanceNode.left;
+        disbalanceNode.left = disbalanceNode.left.right;
+        newRoot.right = disbalanceNode;
+        disbalanceNode.height = 1 + Math.max(getHeight(disbalanceNode.left), getHeight(disbalanceNode.right));
+        newRoot.height = 1 + Math.max(getHeight(newRoot.left), getHeight(newRoot.right));
+        return newRoot;
+    }
+
+    // rotate left
+    private BinaryNode rotateLeft(BinaryNode disbalanceNode) {// tc o1---sc o1
+        BinaryNode newRoot = disbalanceNode.right;
+        disbalanceNode.right = disbalanceNode.right.left;
+        newRoot.left = disbalanceNode;
+        disbalanceNode.height = 1 + Math.max(getHeight(disbalanceNode.left), getHeight(disbalanceNode.right));
+        newRoot.height = 1 + Math.max(getHeight(newRoot.left), getHeight(newRoot.right));
+        return newRoot;
+    }
+
+    // getBalance, identify the condition that cause the unbalance and use the
+    // correct rotation
+    public int getBalance(BinaryNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    private BinaryNode insertNode(BinaryNode node, int nodeValue) {// tc o(1)---sc o(1)
+        if (node == null) {
+            BinaryNode newNode = new BinaryNode();
+            newNode.value = nodeValue;
+            newNode.height = 1;
+            return newNode;
+        } else if (nodeValue < node.value) {
+            node.left = insertNode(node.left, nodeValue);
+        } else {
+            node.right = insertNode(node.right, nodeValue);
+        }
+
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        int balance = getBalance(node);
+        if (balance > 1 && nodeValue < node.left.value) {
+            return rotateRight(node);
+        }
+
+        if (balance > 1 && nodeValue > node.left.value) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+
+        if (balance < -1 && nodeValue > node.right.value) {
+            return rotateLeft(node);
+        }
+
+        if (balance < -1 && nodeValue < node.right.value) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+
+        return node;
+    }
+
+    public void insert(int value) {
+        root = insertNode(root, value);
+    }
+
+    // delete a node
+    public static BinaryNode minimumNode(BinaryNode root) {//tc o logn ---sc 1
+        if (root.left == null) {
+            return root;
+        }
+        return minimumNode(root.left);
+    }
+
+    public BinaryNode deleteNode(BinaryNode node, int value) {//tc o logn---sc o logn
+        if (node == null) {
+            System.out.println("value not found");
+            return node;
+        }
+        if (value < node.value) {
+            node.left = deleteNode(node.left, value);
+        } else if (value > node.value) {
+            node.right = deleteNode(node.right, value);
+        } else {
+            if (node.left != null && node.right != null) {
+                BinaryNode tempNode = node;
+                BinaryNode minimumNodeForRight = minimumNode(tempNode.right);
+                node.value = minimumNodeForRight.value;
+                node.right = deleteNode(node.right, minimumNodeForRight.value);
+            } else if (node.left != null) {
+                node = node.left;
+            } else if (node.right != null) {
+                node = node.right;
+            } else {
+                node = null;
+            }
+        }
+        int balance = getBalance(node);
+        if (balance > 1 && getBalance(node.left) >= 0) {
+            return rotateRight(node);
+        }
+        if (balance > 1 && getBalance(node.left) < 0) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+        if (balance < -1 && getBalance(node.right) <= 0) {
+            return rotateLeft(node);
+        }
+        if (balance < -1 && getBalance(node.right) > 0) {
+            node.right = rotateRight(node.right);
+            return rotateRight(node);
+        }
+        return node;
+    }
+
+    public void delete(int value) {
+        root = deleteNode(root, value);
+    }
+
+    public void deleteAll(){
+        root=null;
+        System.out.println("avl delete totally");
+    }
+
 }
--- section21/Main.java
+++ section21/Main.java
@@ -1,6 +1,14 @@
 class Main{
     public static void main(String[] args) {
         AVL  newAVL= new AVL();
-        
+        newAVL.insert(5);
+        newAVL.insert(5123);
+        newAVL.insert(59);
+        newAVL.insert(57);
+        newAVL.insert(54);
+        newAVL.insert(52);
+        newAVL.insert(510);
+        newAVL.levelOrder(newAVL.root);
+        System.out.println();
     }
 }
